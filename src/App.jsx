8a18ebba961132b8cd302afa8294c64b5c27689f
caceresrene <rene@outlook.my>
2022-04-27 12:13:46 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+	return (
+		<>
+			<h1>Hello Dojo!</h1>
+			<p>Things I need to do:</p>
+			<ol>
+				<li>Learn React</li>
+				<li>Climb Mt. Everest</li>
+				<li>Run a marathon</li>
+				<li>Feed the dogs</li>
+			</ol>
+		</>
+	);
+}
+
+export default App;
